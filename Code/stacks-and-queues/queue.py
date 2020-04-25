cd79acd40e691b9e8bbd2f2e3f873edd76305659
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2020-04-24 18:02:09 -0700

    add queue w/ list

--- Code/stacks-and-queues/queue.py
+++ Code/stacks-and-queues/queue.py
@@ -79,29 +79,46 @@ class ArrayQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.list == []:
+            return True
+        else:
+            return False
+
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.is_empty() is False:
+            return self.list[0]
+        else:
+            return None
+
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+        if self.is_empty() is False:
+            return self.list.pop(0)
+        
+        raise ValueError('No items')
+
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-Queue = LinkedQueue
-# Queue = ArrayQueue
+# Queue = LinkedQueue
+Queue = ArrayQueue
